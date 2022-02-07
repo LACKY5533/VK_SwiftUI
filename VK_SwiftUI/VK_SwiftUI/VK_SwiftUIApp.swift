a38b64fcbc2af27b238e7f88c5c6fe0621a1fbb8
LACKY5533 <atrashkov.nikita@mail.ru>
2022-02-07 18:10:22 +0300

    login sceen

---
+++ VK_SwiftUI/VK_SwiftUI/VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUIApp.swift
+//  VK_SwiftUI
+//
+//  Created by LACKY on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
